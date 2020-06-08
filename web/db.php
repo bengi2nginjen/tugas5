1699f2760df617d7c1294c270755d70a5010b6c0
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-06-09 04:26:40 +0700

    Changing db.php

--- web/db.php
+++ web/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
-$database = getenv('MYSQL_DATABASE');
+$username = "eusebia";
+$password = "tugas5-eusebia";
+$database = "db-tugas5";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Can't connect to database");
