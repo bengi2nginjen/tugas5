2c76137b524059379525a97148a9ca795d8ad8b1
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-06-08 14:57:02 +0700

    new Tugas

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "tugas4.cenzylrufou4.us-east-1.rds.amazonaws.com";
+$username = "root";
+$password = "pass1234";
+$database = "php_mysql_crud";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database) or die("Can't connect to database");
+
+// Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+?>
+<?php
+//session_start();
+
+//$conn = mysqli_connect(
+//  'localhost',
+//  'root',
+//  'password123',
+//  'php_mysql_crud'
+//) or die(mysqli_erro($mysqli));
+
+?>
