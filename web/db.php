22b268932a20698af60b525c37c59f5eb16c2364
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-06-09 05:25:16 +0700

    The new Docker

--- web/db.php
+++ web/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$database = "db-tugas5";
+$password = "tugas5_eusebia";
+$database = "db_tugas5";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Can't connect to database");
